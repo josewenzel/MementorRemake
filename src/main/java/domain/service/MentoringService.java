415c07123566b5af0ccda827cc685203231fae75
Jose Wenzel <devwenzel@gmail.com>
2022-10-21 13:14:42 +0200

    feat: cannot add mentor if mentor does not exist

--- src/main/java/domain/service/MentoringService.java
+++ src/main/java/domain/service/MentoringService.java
@@ -1,5 +1,6 @@
 package domain.service;
 
+import domain.exception.MentorDoesNotExistException;
 import domain.model.Employee;
 import domain.port.repository.EmployeeRepository;
 
@@ -11,8 +12,14 @@ public class MentoringService {
     }
 
     public void addMentor(Employee employee, Employee mentor) {
+        if (isNotAMember(mentor))
+            throw new MentorDoesNotExistException();
         employee.addMentor(mentor);
         employeeRepository.update(employee, employee);
+    }
+
+    private boolean isNotAMember(Employee mentor) {
+        return employeeRepository.get(mentor) == null;
     }
 
     public Employee getMentorOf(Employee employee) {
--- src/test/java/unit/domain/service/MentoringServiceTest.java
+++ src/test/java/unit/domain/service/MentoringServiceTest.java
@@ -1,33 +1,54 @@
 package unit.domain.service;
 
+import domain.exception.MentorDoesNotExistException;
 import domain.model.Employee;
 import domain.port.repository.EmployeeRepository;
 import domain.service.MentoringService;
 import fixture.EmployeeFixture;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class MentoringServiceTest {
     @Mock
     EmployeeRepository employeeRepository;
+    private MentoringService mentoringService;
+
+    @BeforeEach
+    void setUp() {
+        mentoringService = new MentoringService(employeeRepository);
+    }
 
     @Test
     public void requests_employee_and_mentor_to_store_to_add_a_mentor() {
-        MentoringService mentoringService = new MentoringService(employeeRepository);
         Employee anEmployee = new EmployeeFixture().build();
         Employee mentor = new EmployeeFixture().build();
 
+        when(employeeRepository.get(mentor)).thenReturn(mentor);
         mentoringService.addMentor(anEmployee, mentor);
 
         Employee mentoredEmployee = new EmployeeFixture()
                 .withId(anEmployee.id())
                 .withPersonalInformation(anEmployee.personalInformation())
                 .withMentor(mentor).build();
+
         then(employeeRepository).should().update(anEmployee, mentoredEmployee);
+    }
+
+    @Test
+    public void disallow_to_add_a_mentor_to_employee_if_mentor_does_not_exist() {
+        Employee anEmployee = new EmployeeFixture().build();
+        Employee mentor = new EmployeeFixture().build();
+
+        when(employeeRepository.get(mentor)).thenReturn(null);
+        assertThatThrownBy(() -> mentoringService.addMentor(anEmployee, mentor))
+                .isInstanceOf(MentorDoesNotExistException.class);
     }
 }
